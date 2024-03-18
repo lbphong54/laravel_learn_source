eaab8472efb7b066497ece94649b2cda6007fa8c
lbphong54 <teo542003@gmail.com>
2024-03-18 18:17:07 +0700

    first commit

---
+++ 3.Rest API/laravel10-raw/app/Http/Controllers/api/AttendeeController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class AttendeeController extends Controller
+{
+    
+
+    private array $relations = ['user'];
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'destroy']);
+        $this->authorizeResource(Attendee::class, 'attendee');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Event $event)
+    {
+        $attendees = $event->attendees()->latest();
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Event $event)
+    {
+        $attendee = $event->attendees()->create([
+            'user_id' => 1
+        ]);
+
+        return new AttendeeResource($attendee);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event,Attendee $attendee)
+    {
+        return new AttendeeResource($attendee);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Event $event, Attendee $attendee)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $event, Attendee $attendee)
+    {
+        $attendee->delete();
+
+        return response(status: 204);
+    }
+}
---
+++ 3.Rest API/laravel10-raw/app/Http/Controllers/api/EventController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\Sanctum;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+     private array $relations = ['user', 'attendees', 'attendees.user'];
+
+     public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'update', 'destroy']);
+        $this->authorizeResource(Event::class, 'event');
+    }
+
+    public function index()
+    {
+        $query = Event::query();
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach ($relations as $relation) {
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
+        return EventResource::collection(
+            $query->latest()->paginate()
+        );
+    }
+
+    protected function shouldIncludeRelation(string $relation): bool
+    {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+
+        return in_array($relation, $relations);
+    }
+
+    /**
+     * 
+     * 
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return new EventResource($event);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        $event->load('user', 'attendees');
+        return new EventResource($event);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Event $event)
+    {
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ])
+        );
+        return new EventResource($event);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+
+        return response(status: 204);
+    }
+}
